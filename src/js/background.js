d8d52689e8dbacb689d1dd0c5b32ca277f72e5fa
Flavien Busseuil <flavien@classcraft.com>
2020-03-10 13:22:53 -0400

    Update

--- src/js/background.js
+++ src/js/background.js
@@ -1,18 +1,54 @@
+import "../img/icon-34.png";
 import "../img/icon-128.png";
 
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  chrome.browserAction.setBadgeText({ text: request.totalPoints });
-  return true;
-});
+const urlGithubRegex = /^https?:\/\/(?:[^./?#]+\.)?github\.com/;
 
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {});
-  });
-});
+function disableIcon({ tabId }) {
+  chrome.browserAction.setBadgeText({ tabId, text: "-" });
+}
 
-chrome.tabs.onActivated.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {});
+function enableIcon({ tabId }) {}
+
+async function getCurrentTabIdAndUrl() {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      if (tabs.length === 0 || tabs[0].id === chrome.windows.WINDOW_ID_NONE) {
+        return reject();
+      }
+      resolve({ id: tabs[0].id, url: tabs[0].url });
+    });
   });
-});
+}
+
+async function setTotalPoints({ totalPoints }) {
+  try {
+    const { id: tabId } = await getCurrentTabIdAndUrl();
+    enableIcon({ tabId });
+    const text = Number(totalPoints) > 999 ? "999+" : totalPoints;
+    chrome.browserAction.setBadgeText({ tabId, text });
+  } catch (error) {
+    const { id: tabId } = await getCurrentTabIdAndUrl();
+    disableIcon({ tabId });
+  }
+}
+
+async function sendMessageToCurrentTab() {
+  try {
+    const { id, url } = await getCurrentTabIdAndUrl();
+    if (!urlGithubRegex.test(url)) {
+      disableIcon({ tabId: id });
+    }
+    chrome.tabs.sendMessage(id, {});
+  } catch (error) {
+    // disableIcon();
+  }
+}
+
+chrome.runtime.onMessage.addListener(setTotalPoints);
+
+chrome.browserAction.onClicked.addListener(sendMessageToCurrentTab);
+
+chrome.tabs.onActivated.addListener(sendMessageToCurrentTab);
+chrome.tabs.onUpdated.addListener(sendMessageToCurrentTab);
+
+chrome.windows.onFocusChanged.addListener(sendMessageToCurrentTab);
--- src/js/content.js
+++ src/js/content.js
@@ -1,18 +1,45 @@
+function getProjectViewLabels() {
+  const countedColumns = ["To do", "To review", "In progress"].map(e =>
+    e.toLocaleLowerCase()
+  );
+
+  return [...$$(".project-column>.Details h3>span")].reduce((acc, el) => {
+    if (!countedColumns.includes(el.textContent.toLocaleLowerCase())) {
+      return acc;
+    }
+
+    const columnLabels = el
+      .closest(".project-column")
+      .querySelectorAll("article:not(.d-none) .issue-card-label");
+    return [...acc, ...columnLabels];
+  }, []);
+}
+
+function getIssueViewLabels() {
+  return [...$$(".IssueLabel")];
+}
+
 function getTotalPoints() {
   return String(
-    [
-      ...($("[aria-label='Issue']").length > 0
-        ? $$(".IssueLabel")
-        : $$("article:not(.d-none) .issue-card-label"))
-    ]
+    (document.location.href.includes("projects")
+      ? getProjectViewLabels()
+      : getIssueViewLabels()
+    )
       .map(val => Number(val.outerText))
       .filter(Number)
       .reduce((total, num) => total + num, 0)
   );
 }
 
-chrome.runtime.sendMessage({ totalPoints: getTotalPoints() });
-
-chrome.runtime.onMessage.addListener(function(request, sender) {
+function sendTotalPoints() {
   chrome.runtime.sendMessage({ totalPoints: getTotalPoints() });
-});
+}
+
+window.addEventListener("load", () => sendTotalPoints());
+chrome.runtime.onMessage.addListener(() => sendTotalPoints());
+
+// var mutationObserver = new MutationObserver(function(mutations) {
+//   mutations.forEach(function(mutation) {
+//     console.log(mutation);
+//   });
+// });
